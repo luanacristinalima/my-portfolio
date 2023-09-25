a036275e91e42f74e0914e673c33f8d0bc8ceea4
Luana Lima <luana.arqeurb@gmail.com>
2023-09-25 10:34:11 +0100

    about update

--- src/roots/About.js
+++ src/roots/About.js
@@ -17,10 +17,12 @@ export default function About() {
               <h2 className="mb-4">Frontend Developer, based in Portugal.</h2>
               <p className="about-text mb-4">
                 I'm originally from Brazil, but now I'm based in Portugal. I
-                graduated in architecture and urban design, but recently I
-                started to learn code. Since then, I haven't stopped and I'm
-                excited to keep learning and start a new career in information
-                technology.
+                hold a degree in architecture and urban design, and over the
+                past year, I've embarked on a dynamic journey into coding. I've
+                actively pursued frontend development, taking on freelance
+                projects to apply and enhance my skills. This transition has
+                ignited my passion for information technology, and I'm eager to
+                continue learning and forge a new career in this field.
               </p>
               <Link to="/work" title="Luana's work">
                 View my Projects
